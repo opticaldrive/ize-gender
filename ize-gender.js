9b20674309585d0990450ac5861595a1ed82e3c1
a <138826219+opticaldrive@users.noreply.github.com>
2026-07-12 19:02:32 -0700

    oo waow

--- ize-gender.js
+++ ize-gender.js
@@ -1,5 +1,9 @@
 document.body.style.border = "5px solid red"; //leave for test it loads until prod
 const listy = ["example", "domain"];
 
+const par = document.createElement("p");
+par.textContent = "meopw";
+
+document.body.prepend(par);
 // todo: find
 // todo: mark/highlight
