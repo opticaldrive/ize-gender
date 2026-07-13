6b05f879a7bb619f0db646738c05386188f58291
a <138826219+opticaldrive@users.noreply.github.com>
2026-07-12 19:58:02 -0700

    note

--- ize-gender.js
+++ ize-gender.js
@@ -1,7 +1,7 @@
 document.body.style.border = "5px solid red"; //leave for test it loads until prod
 const listy = ["example", "domain"];
 
-const par = document.createElement("mark");
+const par = document.createElement("mark"); // dont use mark use smt else for accessbility ig
 par.textContent = "meopw";
 
 document.body.prepend(par);
