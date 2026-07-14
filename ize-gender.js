c3f2b28e80b60300fc013a1676605563934882c4
a <138826219+opticaldrive@users.noreply.github.com>
2026-07-13 17:46:46 -0700

    er

--- ize-gender.js
+++ ize-gender.js
@@ -1,8 +1,20 @@
 document.body.style.border = "5px solid red"; //leave for test it loads until prod
 const listy = ["example", "domain"];
 
-const par = document.createElement("mark"); // dont use mark use smt else for accessbility ig
-par.textContent = "meopw";
+const par = document.createElement("span"); // dont use mark use smt else for accessbility ig
+const texttest = "example domain domain example test test";
+
+let texttoHighlight = texttest;
+
+listy.forEach(word => {
+    const regex = new RegExp(`\\b${word}\\b`, 'gi');
+    texttoHighlight = texttoHighlight.replace(regex, `<span style="color: blue;">${word}</span>`)
+
+});
+
+// par.textContent = "meopw";
+
+par.innerHTML = texttoHighlight;
 
 document.body.prepend(par);
 // todo: find
