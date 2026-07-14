0b9b8c57dd7d0142d75a1ba8d201c20b972e38bd
a <138826219+opticaldrive@users.noreply.github.com>
2026-07-13 17:57:21 -0700

    goog

--- ize-gender.js
+++ ize-gender.js
@@ -4,11 +4,12 @@ const listy = ["example", "domain"];
 const par = document.createElement("span"); // dont use mark use smt else for accessbility ig
 const texttest = "example domain domain example test test";
 
+// const colours = {"blue": }
 let texttoHighlight = texttest;
 
 listy.forEach(word => {
     const regex = new RegExp(`\\b${word}\\b`, 'gi');
-    texttoHighlight = texttoHighlight.replace(regex, `<span style="color: blue;">${word}</span>`)
+    texttoHighlight = texttoHighlight.replace(regex, `<span style="background-color:  rgba(85, 205, 252, 0.5);">${word}</span>`)
 
 });
 
@@ -19,3 +20,4 @@ par.innerHTML = texttoHighlight;
 document.body.prepend(par);
 // todo: find
 // todo: mark/highlight
+
